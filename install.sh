963c2d818204d33f97366f9757f3f119ebd1dd83
princeyesuraj <princeyesuraj@gmail.com>
2023-05-30 07:50:34 +0530

    add brew site functions in autocomplete

--- install.sh
+++ install.sh
@@ -9,3 +9,14 @@ ln -sf "$HOME/dotfiles/zsh/.zprofile" "$HOME/.config/zsh"
 ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
 rm -rf "$HOME/.config/zsh/external"
 ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
+
+if test "$(uname)" = "Darwin" ; then
+  # MacOS
+  font_dir="$HOME/Library/Fonts"
+else
+  # Linux
+  font_dir="$HOME/.local/share/fonts"
+  mkdir -p $font_dir
+fi
+
+cp $HOME/dotfiles/fonts/* "$font_dir/"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -2,7 +2,8 @@ source "$XDG_CONFIG_HOME/zsh/aliases"
 
 if type brew &>/dev/null; then
     FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
-    autoload -Uz compinit;
+    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+    autoload -Uz compinit
     compinit
 fi
 
